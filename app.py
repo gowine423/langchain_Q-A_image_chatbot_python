56d6bc6feb26f68c61f9c571519f0c6fb4f7f3b0
gowine423 <gowine423@gmail.com>
2024-06-14 20:18:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+# Q&A Chatbot with image
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+from PIL import Image
+import google.generativeai as genai
+
+#Gemini Key
+os.environ['GOOGLE_API_KEY']="AIzaSyB6-jZLBXeOeLFBhFaU11oidwAeBATkrds"
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+## Function to load OpenAI model and get respones
+def get_gemini_response(input,image,prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+st.set_page_config(page_title="Gemini Image Demo")
+st.header("SURAT Q&A ChatBot :books:")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me")
+input_prompt = """
+               You are an expert in understanding invoices.
+               You will receive input images as invoices &
+               you will have to answer questions based on the input image
+               """
+
+## If ask button is clicked
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
