8cbef33eae58dd509e70d6844299755b4e957995
gowine423 <gowine423@gmail.com>
2024-07-22 17:32:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 ## Function to load OpenAI model and get respones
 def get_gemini_response(input,image,prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-pro')
     response = model.generate_content([input,image[0],prompt])
     return response.text
     
